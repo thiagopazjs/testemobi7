4fcfa185311a5a5b8f2527b5ce271ef2f1f548f5
Thiagopazjs <thiagopaz907@gmail.com>
2022-02-28 11:24:36 -0300

    Created Posicoes Repository

---
+++ src/main/java/com/mobi7/mobi7/repository/PosicaoRepository.java
@@ -0,0 +1,7 @@
+package com.mobi7.mobi7.repository;
+
+import com.mobi7.mobi7.entity.Posicoes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PosicaoRepository extends JpaRepository <Posicoes, Long> {
+}
